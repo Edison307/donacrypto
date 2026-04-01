770c5f37aa2c01a574d731fa1163652e30dd5f73
Edison307 <edisonfsoares@hotmail.com>
2026-04-01 05:28:49 -0300

    struct campanha

---
+++ contracts/inicio.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity ^0.8.17; 
+
+struct Campaign{
+   address author;
+   string title;
+   string description;
+   string videoUrl;
+   string imageUrl;
+   uint256 balance;
+   bool active;
+}
